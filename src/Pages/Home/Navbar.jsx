7a72903878840d78afda6e6eee66d9bb84da1688
mikel <mikel.urrestarazu@gmail.com>
2023-12-02 09:53:07 +0100

    Debugging errors

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -11,23 +11,24 @@ const closeMenu =() =>{
     setNavActive(false)
 }
 
-useEffect(()=>{
-    const handleResize = () =>{
-        if(window.innerWidth <=500){
-            closeMenu
-        }
-    }
-    window.addEventListener("resize",handleResize)
-    return ()=> {
-        window.removeEventListener("resize",handleResize)
-    }
-},[]);
+useEffect(() => {
+  const handleResize = () => {
+      if (window.innerWidth <= 500) {
+          closeMenu();
+      }
+  }
+  window.addEventListener("resize", handleResize);
+  return () => {
+      window.removeEventListener("resize", handleResize);
+  }
+}, []);
 
-useEffect(()=>{
-    if(window.innerWidth <=1200){
-        closeMenu
-    }
-},{})
+useEffect(() => {
+  if (window.innerWidth <= 1200) {
+      closeMenu();
+  }
+}, []);
+
 return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       <div>
