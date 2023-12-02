2f5c64bf37859c00cb2f61129c79c0fa10f57465
mikel <mikel.urrestarazu@gmail.com>
2023-12-02 09:35:53 +0100

    portfolio structure added

---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,23 @@
+import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
+import Testimonial from "../Testimonial";
+import ContactMe from "../ConctacMe";
+import Footer from "../Footer";
+
+const Home = () => {
+    return (
+        <>
+            <HeroSection />
+            <MySkills />
+            <AboutMe />
+            <MyPortfolio />
+            <Testimonial />
+            <ContactMe />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
